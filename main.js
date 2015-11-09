6511e4b09e5a9448686226edf1e3025e70d33242
Kim Hart <kimhart90@gmail.com>
2015-11-08 19:42:13 -0500

    centered cards

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 
 // STUPID STUPID NOT-DRY SHAMELESS CODE //
-// SETTING UP ORIGINAL CARD VARIABLES //
 
 var card1 = document.querySelector("#card1");
 var card2 = document.querySelector("#card2");
@@ -12,15 +11,6 @@ var card6 = document.querySelector("#card6");
 var card7 = document.querySelector("#card7");
 var card8 = document.querySelector("#card8");
 
-// Now we want to be able to assign the class of 'character' to all of the cards RANDOMLY when the page loads. We need to make an array of objects that include 2 photos of each character, with their name and imgsrc as properties. This also needs to function on the shuffle command, which will be called when the player clicks 'reset board' or 'try level again' //
-
-
-
-
-var startNewGame = document.querySelector(".new-game-button");
-
-
-// SHUFFLE THROUGH THE CARDS //
 
 var characterList = [ 
 		{name: "harry", img: "images/harry.png"}, 
@@ -28,7 +18,9 @@ var characterList = [
 		{name: "hermione", img: "images/hermione.png"},
 		{name: "hermione2", img: "images/hermione.png"},
 		{name: "ron", img: "images/ron.png"},
-		{name: "ron2", img: "images/ron.png"}
+		{name: "ron2", img: "images/ron.png"},
+		{name: "dobby", img: "images/dobby.png"},
+		{name: "dobby2", img: "images/dobby.png"}
 		];
 
 
@@ -42,48 +34,46 @@ function shuffleArray(array) {
     return characterList;
 };
 
-// run shuffle function on click:
-// startNewGame.addEventListener("click", shuffle);
+
+var startNewGame = document.querySelector(".new-game-button");
+startNewGame.addEventListener("click", shuffleArray);
+
+// var assignNewCharacters = function() {
+//  	card1.classList.add('character');
+// }
+// console.log(assignNewCharacters);
 
 
 
-// CLICKING CARDS DURING THE GAME //
 
 var flipCard1 = function(event){
 	card1.classList.remove("facedown");
     console.log(event);
 };
-
 var flipCard2 = function(event){
 	card2.classList.remove("facedown");
     console.log(event);
 };
-
 var flipCard3 = function(event){
 	card3.classList.remove("facedown");
     console.log(event);
 };
-
 var flipCard4 = function(event){
 	card4.classList.remove("facedown");
     console.log(event);
 };
-
 var flipCard5 = function(event){
 	card5.classList.remove("facedown");
     console.log(event);
 };
-
 var flipCard6 = function(event){
 	card6.classList.remove("facedown");
     console.log(event);
 };
-
 var flipCard7 = function(event){
 	card7.classList.remove("facedown");
     console.log(event);
 };
-
 var flipCard8 = function(event){
 	card8.classList.remove("facedown");
     console.log(event);
